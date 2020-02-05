10721984135d160255aa6c6496c4bfe151aca650
minoring <minho1350@gmail.com>
2020-02-04 22:29:01 -0800

    Implement loss functions

--- ml-from-scratch/mlfromscratch/utils/data_operation.py
+++ ml-from-scratch/mlfromscratch/utils/data_operation.py
@@ -2,6 +2,12 @@ import numpy as np
 
 
 def mean_squared_error(y_true, y_pred):
-    """Returns the mean squared error between y_true and y_pred"""
-    mse = np.mean(np.power(y_true - y_pred, 2))
-    return mse
+  """Returns the mean squared error between y_true and y_pred"""
+  mse = np.mean(np.power(y_true - y_pred, 2))
+  return mse
+
+
+def accuracy_score(y_true, y_pred):
+  """Compare y_true to y_pred and return the accuracy"""
+  accuracy = np.sum(y_true == y_pred, axis=0) / len(y_true)
+  return accuracy
